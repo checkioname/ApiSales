d075c5cf530e29c86c2c290b17805a538eb546c0
checkioname <lucasgargalhone@gmail.com>
2024-05-10 01:40:55 -0300

    CODE: more tests implemented

--- PROJETO-JOAQUIM/src/test/java/com/example/demo/services/PaisServiceTest.java
+++ PROJETO-JOAQUIM/src/test/java/com/example/demo/services/PaisServiceTest.java
@@ -1,11 +1,13 @@
 package com.example.demo.services;
 
 import com.example.demo.repository.PaisRepository;
+import com.example.demo.services.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.example.demo.domain.Pais;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -13,6 +15,11 @@ import org.mockito.MockitoAnnotations;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 class PaisServiceTest {
 
     private PaisService service;
@@ -46,14 +53,26 @@ class PaisServiceTest {
 
     @Test
     void insert() {
+        Pais pais = new Pais("123","Brasil","America do Sul","1000000");
+        Mockito.when(repository.insert((Pais) Mockito.any())).thenReturn(pais);
+        Pais response = service.insert(Mockito.any());
+        Assertions.assertEquals(response,pais);
     }
 
     @Test
     void fromDTO() {
     }
 
+    @Test()
+    void shouldReturnExceptionWhenDeleteUserIdNull(){
+        assertThrows(ObjectNotFoundException.class , ()-> service.delete(null));
+    }
+
     @Test
     void delete() {
+        repository.deleteById(anyString());
+        repository.deleteById(anyString());
+        verify(repository, times(2)).deleteById(anyString());
     }
 
     @Test
